10f4c6c3a08f207d58dc02890629bbdbc4668ed6
Maialinda3007 <108538836+Maialinda3007@users.noreply.github.com>
2024-08-21 12:31:22 -0300

    s

---
+++ main.js
@@ -0,0 +1,16 @@
+const perguntas=[
+ {
+    enunciado:"Insira o enunciado da Pergunta 1",
+    alternativas: [
+ {
+   texto:"Alternativa 1",
+   afirmação:"Afirmação da alternativa 1"
+},
+{
+    texto:"Alternativa 2",
+    afirmação:"Afirmação da alternativa 2"
+}
+    ]
+
+
+
